843d529ddc8fe04b257e652c22c2777740ed1454
nicogutierrezcoll <gutierrezcolln@.com>
2024-06-08 11:06:37 -0300

    first commit

--- exercices-level-1/exercice-1.py
+++ exercices-level-1/exercice-1.py
@@ -1,3 +1,14 @@
 '''Definir una función max() que tome como argumento dos números y devuelva el mayor de ellos.
 Es cierto que python tiene una función max() incorporada, pero hacerla nosotros mismos es un
 muy buen ejercicio'''
+
+
+def num_maximo(num1, num2):
+    if num1 > num2:
+        return f"El numero maximo es: {num1}"
+    else:
+        return f"El numero maximo es: {num2}"
+
+
+resultado = num_maximo(10, 25)
+print(resultado)
